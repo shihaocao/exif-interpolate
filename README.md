196a0fa09ad678f4c54ba2898b3615e5e5a28b22
Shihao Cao <shehowcow@gmail.com>
2021-01-18 20:50:03 -0500

    more work

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # exif-interpolate
 A project to batch edit exif data interpolated over a date range - To give order Google Photos uploads
 
+Tested working on Python 3.8.5
 ### PRE-OPS
 
-I used `mogrify` to change all png files to jpg files just to simplify things.
+I used `mogrify` to change all png files to jpg files just to simplify things. This package should be available via
+```
+sudo apt-get install imagemagick
+```
+
+### INSTALL
+
+Use a virtualenv if you want:
+```
+python3 -m venv venv
+. venv/bin/activate
+```
+
+Install libraries:
+```
+pip install -r requirements.txt
+```
 
 ### RUN
+
+Example: 
+
+```
+python3 batch-edit.py --input ~/FB/JP2
+```
--- batch-edit.py
+++ batch-edit.py
@@ -1,19 +1,26 @@
 from os import listdir
 from os.path import isfile, join
 import argparse
+import exif
 
 def main(args):
     mypath = args["input"]
     
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print(onlyfiles)
-    print(args['mutate'])
+    input_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    input
+    for file_name in input_files:
+        with open(mypath+file_name, 'rb') as image_file:
+            if file_name.endswith('.mp4'):
+                print("mp4 rip")
+            elif file_name.endswith('.jpg'):
+                unpacked = exif.Image(image_file)
+                print(unpacked.has_exif)
+            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='provide arguments for batch convert')
 
-    # agent parameters
-    parser.add_argument('--input', help='input dir', default="./input/")
+    parser.add_argument('--input', help='input dir, please use absolute, with trailing slash', default="./input/")
     parser.add_argument('--mutate', help='If true, it will modify the exif data in place', default=True)
 
     args = vars(parser.parse_args())
